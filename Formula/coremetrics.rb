abddb3f7df5d994c7b7d39782fae8d4ad563f138
Sviatoslav Oleksiienko <soleksiienko1@gmail.com>
2026-06-15 02:43:14 -0400

    coremetrics: bump to v0.2.9

--- Formula/coremetrics.rb
+++ Formula/coremetrics.rb
@@ -2,18 +2,18 @@ class Coremetrics < Formula
   desc "Cross-platform desktop system metrics monitor (CPU, RAM, GPU, processes)"
   homepage "https://github.com/sviatil0/coremetrics"
   license "LGPL-2.1-only"
-  version "0.2.8"
+  version "0.2.9"
 
   on_macos do
     on_arm do
-      url "https://github.com/sviatil0/coremetrics/releases/download/v0.2.8/coremetrics-v0.2.8-macos-arm64.tar.gz"
-      sha256 "d1406aca76219c62d85ac14be6116eddcce3033fd15eaac32f9ec335f47374b4"
+      url "https://github.com/sviatil0/coremetrics/releases/download/v0.2.9/coremetrics-v0.2.9-macos-arm64.tar.gz"
+      sha256 "8258279a85cdd989eb19f7f31650f32ababade546abf394805de4e4482b3aeb8"
     end
   end
 
   on_linux do
-    url "https://github.com/sviatil0/coremetrics/releases/download/v0.2.8/coremetrics-v0.2.8-linux-x86_64.tar.gz"
-    sha256 "64085b5a43c227ed17ad327db1256480d31f34e2ce955d3653453440ecf48392"
+    url "https://github.com/sviatil0/coremetrics/releases/download/v0.2.9/coremetrics-v0.2.9-linux-x86_64.tar.gz"
+    sha256 "32cec14d61ef26083df61822ca81cbfd2c4cf228afcf1fdc6738c91a34ec3ff2"
   end
 
   depends_on "sdl3"
